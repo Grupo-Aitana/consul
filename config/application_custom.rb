8ef09cde2425a9e6a3eecfb2f2aff4e77896a8a1
Samuel Garcia <samuel.garcia@aitana.es>
2018-06-12 09:22:30 +0200

    cambios en la interfaz

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,7 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = :val
+    config.i18n.available_locales = [:en, :es, :val]
+    config.i18n.fallbacks = {'val' => 'es', 'en' => 'es'}
   end
 end
