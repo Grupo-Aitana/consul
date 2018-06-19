a2dcc9831bf01d65467ca8a3ff7a7e132ca8b83e
Fernando Faubell Valls <ffaubell@fadrell.com>
2018-06-19 11:50:45 +0200

    Votaciones en Gestión.

--- config/routes/management.rb
+++ config/routes/management.rb
@@ -48,4 +48,18 @@ namespace :management do
       get :print, on: :collection
     end
   end
+
+  # Funcionalidad nueva para Guadassuar
+  # Desde gestión hay que poder votar.
+  resources :polls, only: [:show, :index] do
+    member do
+      get :stats
+      get :results
+    end
+
+    resources :questions, controller: 'polls/questions', shallow: true do
+      post :answer, on: :member
+    end
+  end
+
 end
