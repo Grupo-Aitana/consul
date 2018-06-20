4347c9d9814f16682988b9c0a20d2bae6db3c0d7
Samuel Garcia <samuel.garcia@aitana.es>
2018-06-20 10:16:17 +0200

    cambios en managed polls

--- app/controllers/management/polls_controller.rb
+++ app/controllers/management/polls_controller.rb
@@ -16,11 +16,11 @@ class Management::PollsController < Management::BaseController
 
   def show
     @questions = @poll.questions.for_render.sort_for_list
-    @token = poll_voter_token(@poll, current_user)
+    @token = poll_voter_token(@poll, managed_user)
     @poll_questions_answers = Poll::Question::Answer.where(question: @poll.questions).where.not(description: "").order(:given_order)
 
     @answers_by_question_id = {}
-    poll_answers = ::Poll::Answer.by_question(@poll.question_ids).by_author(current_user.try(:id))
+    poll_answers = ::Poll::Answer.by_question(@poll.question_ids).by_author(managed_user.try(:id))
     poll_answers.each do |answer|
       @answers_by_question_id[answer.question_id] = answer.answer
     end
