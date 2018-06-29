d5ed466032ff1b7a43fd09bad2724cd351419645
Fernando Faubell Valls <fernando.faubell@aitana.es>
2018-06-29 21:51:01 +0200

    Configuración locales y reinicio en la implantación.

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,7 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = :val
+    config.i18n.available_locales = [:val, :es, :en, :fr, :nl, 'pt-BR']
+    config.i18n.fallbacks = {'val' => 'es', 'fr' => 'es', 'pt-br' => 'es', 'nl' => 'en'}
   end
 end
