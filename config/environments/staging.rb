002ece14b748aebb25b35e0af52af6b270e2a374
Sandra <sandraescobedobadia@gmail.com>
2018-11-20 15:13:32 +0100

    configure: activate dalli gem for caching

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,7 +2,8 @@ Rails.application.configure do
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
+  config.cache_store = :dalli_store
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 'https' }
   config.action_mailer.asset_host = "https://localhost"
@@ -12,5 +13,4 @@ Rails.application.configure do
   config.assets.debug = true
   config.assets.digest = true
   config.assets.raise_runtime_errors = true
-  config.cache_store = :dalli_store
 end
